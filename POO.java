9fa7b5a3718c3de0efe52beda6a3b3d4412d6ebe
Nati-Guzman <124587922+Nati-Guzman@users.noreply.github.com>
2023-05-23 14:57:22 -0500

    Add files via upload

---
+++ POO.java
@@ -0,0 +1,37 @@
+/******************************************************************************
+Abril 10/2023
+Intoducción a Java
+POO.Java
+POO.class
+java POO
+
+*******************************************************************************/
+
+//El archivo debe llamarse igual que la Clase
+
+//Clase debe iniciar con mayuscula
+ class Main //file name = Main.java
+
+{
+public static void main(String[] args) {
+System.out.println("Hello World");
+}
+}
+
+
+//Instanciar las clases = crear objetos
+
+class Person{
+    string name;
+    int age;
+   
+    //Método
+    void birthday(){
+        age ++;
+        System.out.println(name + "is now "+ age);
+    }
+};
+
+
+
+//¿Como ordenar un arreglo y encontrar un elemento dentro del arreglo
